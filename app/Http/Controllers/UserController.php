acf5f8f3b81f32529fd2df927aac3f875415069c
Guriya Kumari <guri2000kumari@gmail.com>
2022-09-14 16:22:35 +0530

    login and admin pannel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,8 +63,50 @@ class UserController extends Controller
     	}
 
     }
+    //handle the login from ajax
+
+    public function loginUser(Request $request){
+
+      $validator = Validator::make($request->all(),[
+      'email'=>'required|email|max:100',
+        'password'=>'required|min:6|max:50'
+    ]);
+
+  if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'messages'=>$validator->getMessageBag()
+            ]);
+    }else{
+        $user = User::where('email',$request->email)->first();
+        if($user){
+            if(Hash::check($request->password, $user->password)){
+                $request->session()->put('loggedInUser',$user->id);
+                return response()->json([
+
+                    'status'=>200,
+                    'messages'=>'success'
+                ]);
+            }else{
+                return response()->json([
+                    'status'=>401,
+                    'messages'=>'E-mail or password is incorrect'
+
+                ]);
+              }
+            }else{
+                return response()->json([
+                    'status'=>401,
+                    'messages'=>'User not found!'
+                ]);
+            }
+        }
+    }
 
 
+  public function profile(){
+    return view('layouts.dashboard');
+  }
 }
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,6 +11,7 @@
 				   <h2 class="fw-bold text-secondary">Register</h2>
 			    </div>
 			    <div class="card-body p-5">
+			    	<div id="show_success_alert">
 			    	<form action="#" method="POST" id="register_form">
 			    		@csrf
 			    		<div class="mb-3">
@@ -42,6 +43,7 @@
 
 			    	</form>
 			    </div>
+			    </div>
 		    </div>
 	    </div>
    <!--  </div> -->
@@ -56,14 +58,26 @@
 $(function(){
 	$("#register_form").submit(function(e){
 	e.preventDefault();
-	$("#register_btn").val('please wait....');
+	$("#register_btn").val('Register');
 	$.ajax({
 		url:'{{route('auth.register')}}',
 		method:'post',
 		data:$(this).serialize(),
 		dataType:'json',
 		success:function(res){
-			console.log(res);
+			if(res.status==400){
+				showError('name',res.messages.name);
+				showError('email',res.messages.email);
+				showError('password',res.messages.password);
+				showError('cpassword',res.messages.cpassword);
+				$("#register_btn").val('Register');
+
+			}else if(res.status==200){
+				$("#show_success_alert").html(showMessage('success',res.messages));
+				$("#register_form")[0].reset();
+				removeValidationClass("#register_form");
+				$("#register_btn").val('Register');
+			}
 		}
 
       });
